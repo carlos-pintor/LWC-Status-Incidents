8465f768a4d829e3e416f701c4d74d95c0157e0c
Carlos Pintor <36550078+carlos-pintor@users.noreply.github.com>
2020-11-03 13:24:42 -0600

    Adds lightning record view form to display record fields

--- force-app/main/default/lwc/status_incidents/status_incidents.js
+++ force-app/main/default/lwc/status_incidents/status_incidents.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class Status_incidents extends LightningElement {}
+export default class Status_incidents extends LightningElement {
+    @api recordId;
+}
