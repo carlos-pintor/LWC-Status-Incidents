4333decf7af02c95ecf8572b7c9fcee72ada9620
Carlos Pintor <36550078+carlos-pintor@users.noreply.github.com>
2020-11-08 14:00:24 -0600

    getRecord wired function added

--- force-app/main/default/lwc/status_incidents/status_incidents.js
+++ force-app/main/default/lwc/status_incidents/status_incidents.js
@@ -1,5 +1,22 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+const FIELDS = [NAME_FIELD];
 
 export default class Status_incidents extends LightningElement {
     @api recordId;
+    @api daysBack;
+
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    wiredRecord({ error, data }) {
+        if(data){
+            console.log("data:", data);
+            console.log("daysBack:", this.daysBack);
+            let startDate = new Date();
+            startDate.setDate(startDate.getDate() - this.daysBack);
+            startDate = startDate.toISOString().slice(0,10);
+            console.log("startDate: " + startDate);
+        }
+    }
 }
